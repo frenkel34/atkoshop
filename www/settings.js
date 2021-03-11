d8e0b5a7862e1975484de21dc078f0002cdd8180
Frenkel <benusfrank@gmail.com>
2021-03-11 08:00:16 +0100

    Added setting for running on localhost

--- www/settings.js
+++ www/settings.js
@@ -1,7 +1,7 @@
 window.settings = {
 	"okta": {
-		"oktaurl": "https://finance.atkoinc.nl/",
-		"clientid": "0oa1cewk0vC82i4kZ0x7",
+		"oktaurl": "https://auth.atkoinc.nl",
+		"clientid": "0oa5i4euoIg19ZWRq416",
 		"authorizationserver": "default",
 		"basic_scopes": "openid profile",
 		"access_low_scope": "access_low",
@@ -9,9 +9,12 @@ window.settings = {
 	},
 	"urls": {}, // See settings below
 	"app": {
-		"name": "Atko Finance",
-		"color": "#0000ff",
-		"title": "Atko Insurance",
+		"run_local": true,
+		"local_address": "http://localhost:8000",
+		"hosted_address": "https://atkoshop.netlify.app",
+		"name": "Atko Shop",
+		"color": "#ff4486",
+		"title": "Atko Shop: Till u drop!",
 		"logging": true,
 		"yourname": 'Frank',
 		"buy_label": "Buy Now",
@@ -55,9 +58,14 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|
 		window.settings.urls.oktalogout 	= settings.okta.oktaurl + '/login/signout?fromURI=https://www.atkoinc.nl/app/?message=okta session ended'
 	} else {
 		window.settings.platform 			= 'browser';
-		window.settings.urls.url 			= 'https://atkoinsurance.netlify.app';
-		window.settings.urls.renewurl 		= 'https://atkoinsurance.netlify.app';
-		window.settings.urls.callbackurl 	= 'https://atkoinsurance.netlify.app';
+		if (settings.app.run_local == true) {
+			browser_address = window.settings.app.local_address
+		} else {
+			browser_address = window.settings.app.hosted_address
+		}
+		window.settings.urls.url 			= browser_address;
+		window.settings.urls.renewurl 		= browser_address;
+		window.settings.urls.callbackurl 	= browser_address;
 		window.settings.urls.oktalogout 	= settings.okta.oktaurl + '/login/signout?fromURI='+ settings.urls.url;
 	}
 
@@ -68,25 +76,25 @@ if (settings.app.logging) {
 
 // Product settings
 productArray = [{
-	"title": "Retirement services",
-	"subtitle": "Rest insured and covered",
-	"image": "https://images.pexels.com/photos/1642883/pexels-photo-1642883.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	"intro": "Our services specialy design for both young professionals as well as senior citizens, basicly everybody.",
-	"price": '',
+	"title": "X556TX-33",
+	"subtitle": "Model year 2024",
+	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-sports-cars/10-2018-bmw-3-series-m3_evox.jpg",
+	"intro": "This latest model is now available for pre-order, is brand new. With 10k HP ...",
+	"price": '$ 115.000',
 	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at efficitur mauris. Quisque eros nulla, pulvinar ac efficitur non, tempor at ante. Etiam facilisis molestie magna, gravida maximus massa molestie ut. Nunc ac velit finibus, blandit purus sed, tempor erat. Aliquam posuere augue scelerisque rutrum congue. Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu. Nunc quis nulla rhoncus, convallis lectus id, posuere erat. Integer sit amet metus nec risus eleifend porta nec eget odio."
 }, {
-	"title": "Health insurance",
-	"subtitle": "COVID-19 Vaccin covered",
-	"image": "https://images.pexels.com/photos/4506109/pexels-photo-4506109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	"intro": "We have you covered, in these crazy times as well as any other time, we even have a 99% discount on first surgery.",
-	"price": '',
+	"title": "XST3 GT",
+	"subtitle": "Tomorrows future on four wheels",
+	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-awd-cars/07-2018-dodge-charger-rt_evox_newnew.jpg",
+	"intro": "The latest addition to the XST family at Audi and never as fast as this one. Offered with a online only 'no test-drive' discount ...",
+	"price": '$ 44.000',
 	"description": "Proin elementum eget tellus at imperdiet. Phasellus pellentesque vulputate fringilla. Proin suscipit eleifend ligula sit amet porttitor. Etiam rhoncus purus magna, vitae dictum massa convallis non. Ut venenatis iaculis congue. Quisque blandit consequat lacus. Nulla tellus massa, egestas sed lacus eget, pharetra elementum orci. Cras placerat gravida cursus. Quisque dignissim rhoncus justo, nec ullamcorper magna. Aliquam volutpat lacus a eros eleifend cursus. Curabitur luctus enim turpis, nec venenatis nibh euismod et. Praesent vel enim ac lectus fermentum maximus a vitae eros. Morbi sagittis mi arcu, et fermentum augue viverra id. Donec luctus, sem sit amet mattis feugiat, ligula tellus sollicitudin mauris, quis fringilla risus tortor eget felis. Aliquam et viverra mauris."
 }, {
-	"title": "Car insurance",
-	"subtitle": "All risk coverage 30% off",
-	"image": "https://images.pexels.com/photos/620335/pexels-photo-620335.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-	"intro": "You are covered from open roads to dense city streets, your new sportscar as well as classics and junk-cars.",
-	"price": '',
+	"title": "Proto type 21",
+	"subtitle": "Arnold Schwarzenegger version",
+	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-sports-cars/08-mazda-mx-5-miata--new.jpg",
+	"intro": "The newest model in a line of older models that will blow your mind and knock you off you feet, for real...",
+	"price": '$21.500',
 	"description": "Duis eleifend rhoncus odio quis ornare. Ut a tincidunt arcu. Cras suscipit at augue eget euismod. Maecenas imperdiet porta lorem sed vestibulum. Phasellus massa leo, laoreet non turpis in, efficitur sagittis mauris. Etiam maximus tincidunt velit, at accumsan arcu lacinia eget. Cras sodales purus feugiat mi ullamcorper aliquam. In dictum dui dui. Vivamus ac efficitur dui. Phasellus porta quis nisl nec auctor."
 }];
 
