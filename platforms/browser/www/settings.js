f644b23d0703562889aadb141b20beb0226eef1a
Frenkel <benusfrank@gmail.com>
2021-03-19 10:31:19 +0100

    Settings changed

--- platforms/browser/www/settings.js
+++ platforms/browser/www/settings.js
@@ -1,7 +1,7 @@
 window.settings = {
 	"okta": {
-		"oktaurl": "https://auth.atkoinc.nl",
-		"clientid": "0oa5i4euoIg19ZWRq416",
+		"oktaurl": "https://account.atkoinc.nl",
+		"clientid": "0oaxtw9c1nZuHph6c0h7",
 		"authorizationserver": "default",
 		"basic_scopes": "openid profile",
 		"access_low_scope": "access_low",
@@ -11,20 +11,20 @@ window.settings = {
 	"app": {
 		"run_local": true,
 		"local_address": "http://localhost:8000",
-		"hosted_address": "https://atkoshop.netlify.app",
-		"name": "Atko Shop",
-		"color": "#ff4486",
-		"title": "Atko Shop: Till u drop!",
+		"hosted_address": "https://atkotrucks.netlify.app",
+		"name": "Atko Trucks",
+		"color": "#1251b5",
+		"title": "Webshop: Atko",
 		"logging": true,
 		"yourname": 'Frank',
 		"buy_label": "Buy Now",
-		"pay_label": "Pay up",
+		"pay_label": "Pay",
 		"ecommerce": true,
 		"images": [
-			"https://www.carmax.com/~/media/images/carmax/com/articles/best-luxury-cars/07-2018-mercedes-benz-c-class-amg-c63-s.jpg",
-			"https://www.carmax.com/~/media/images/carmax/com/articles/best-luxury-cars/09-2018-audi-s5-sportback-premium-plus_evox.jpg",
-			"https://www.carmax.com/~/media/images/carmax/com/articles/best-luxury-cars/05-2020-bmw-4-series-m4_evox.jpg",
-			"https://www.carmax.com/~/media/images/carmax/com/articles/best-luxury-cars/04-2018-porsche-panamera-4s-executive_12595_cc1280_032_0l.jpg"
+			"https://www.volvotrucks.nl/content/dam/volvo/volvo-trucks/masters/press-releases/2020/feb/pr-2952-range/1860x1050-R2A8182.jpg",
+			"https://www.volvotrucks.nl/content/dam/volvo/volvo-trucks/masters/press-releases/2020/feb/pr-2952-fmx/banner.jpg",
+			"https://www.volvotrucks.nl/content/dam/volvo/volvo-trucks/masters/press-releases/2020/feb/pr-2952-fh/high-res-08A3977-FH-4x2-semi-trailer-long-haul-on-road.jpg",
+			"https://www.volvotrucks.nl/content/dam/volvo/volvo-trucks/masters/press-releases/2020/feb/pr-2952-awareness/1860x1050-VTT_komp1_V001.jpg"
 			],
 		"headers": {
 			"profile": {
@@ -76,26 +76,26 @@ if (settings.app.logging) {
 
 // Product settings
 productArray = [{
-	"title": "X556TX-33",
-	"subtitle": "Model year 2024",
-	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-sports-cars/10-2018-bmw-3-series-m3_evox.jpg",
-	"intro": "This latest model is now available for pre-order, is brand new. With 10k HP ...",
+	"title": "Atko FH",
+	"subtitle": "Designed for international transport",
+	"image": "https://www.volvotrucks.nl/nl-nl/trucks/trucks/new-heavy-duty-range/_jcr_content/root/responsivegrid/layoutcontainer_copy/columncontrol/parsys1/teaser.coreimg.jpeg/1582975923115/volvo-trucks-features-volvo-fh-frontview.jpeg",
+	"intro": "Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu.",
 	"price": '$ 115.000',
 	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at efficitur mauris. Quisque eros nulla, pulvinar ac efficitur non, tempor at ante. Etiam facilisis molestie magna, gravida maximus massa molestie ut. Nunc ac velit finibus, blandit purus sed, tempor erat. Aliquam posuere augue scelerisque rutrum congue. Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu. Nunc quis nulla rhoncus, convallis lectus id, posuere erat. Integer sit amet metus nec risus eleifend porta nec eget odio."
 }, {
-	"title": "XST3 GT",
-	"subtitle": "Tomorrows future on four wheels",
-	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-awd-cars/07-2018-dodge-charger-rt_evox_newnew.jpg",
-	"intro": "The latest addition to the XST family at Audi and never as fast as this one. Offered with a online only 'no test-drive' discount ...",
-	"price": '$ 44.000',
-	"description": "Proin elementum eget tellus at imperdiet. Phasellus pellentesque vulputate fringilla. Proin suscipit eleifend ligula sit amet porttitor. Etiam rhoncus purus magna, vitae dictum massa convallis non. Ut venenatis iaculis congue. Quisque blandit consequat lacus. Nulla tellus massa, egestas sed lacus eget, pharetra elementum orci. Cras placerat gravida cursus. Quisque dignissim rhoncus justo, nec ullamcorper magna. Aliquam volutpat lacus a eros eleifend cursus. Curabitur luctus enim turpis, nec venenatis nibh euismod et. Praesent vel enim ac lectus fermentum maximus a vitae eros. Morbi sagittis mi arcu, et fermentum augue viverra id. Donec luctus, sem sit amet mattis feugiat, ligula tellus sollicitudin mauris, quis fringilla risus tortor eget felis. Aliquam et viverra mauris."
+	"title": "Atko FH16",
+	"subtitle": "Powerful and elegant",
+	"image": "https://www.volvotrucks.nl/nl-nl/trucks/trucks/new-heavy-duty-range/_jcr_content/root/responsivegrid/layoutcontainer_copy/columncontrol/parsys2/teaser.coreimg.jpeg/1582975932582/volvo-trucks-features-volvo-fh16-frontview.jpeg",
+	"intro": "Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu.",
+	"price": '$ 144.000',
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at efficitur mauris. Quisque eros nulla, pulvinar ac efficitur non, tempor at ante. Etiam facilisis molestie magna, gravida maximus massa molestie ut. Nunc ac velit finibus, blandit purus sed, tempor erat. Aliquam posuere augue scelerisque rutrum congue. Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu. Nunc quis nulla rhoncus, convallis lectus id, posuere erat. Integer sit amet metus nec risus eleifend porta nec eget odio."
 }, {
-	"title": "Proto type 21",
-	"subtitle": "Arnold Schwarzenegger version",
-	"image": "https://www.carmax.com/~/media/images/carmax/com/articles/best-sports-cars/08-mazda-mx-5-miata--new.jpg",
-	"intro": "The newest model in a line of older models that will blow your mind and knock you off you feet, for real...",
-	"price": '$21.500',
-	"description": "Duis eleifend rhoncus odio quis ornare. Ut a tincidunt arcu. Cras suscipit at augue eget euismod. Maecenas imperdiet porta lorem sed vestibulum. Phasellus massa leo, laoreet non turpis in, efficitur sagittis mauris. Etiam maximus tincidunt velit, at accumsan arcu lacinia eget. Cras sodales purus feugiat mi ullamcorper aliquam. In dictum dui dui. Vivamus ac efficitur dui. Phasellus porta quis nisl nec auctor."
+	"title": "Atko FM",
+	"subtitle": "Comfort for on the road",
+	"image": "https://www.volvotrucks.nl/nl-nl/trucks/trucks/new-heavy-duty-range/_jcr_content/root/responsivegrid/layoutcontainer_4627/columncontrol/parsys1/teaser.coreimg.jpeg/1582975946767/volvo-trucks-features-volvo-fm-frontview.jpeg",
+	"intro": "Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu.",
+	"price": '$221.500',
+	"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at efficitur mauris. Quisque eros nulla, pulvinar ac efficitur non, tempor at ante. Etiam facilisis molestie magna, gravida maximus massa molestie ut. Nunc ac velit finibus, blandit purus sed, tempor erat. Aliquam posuere augue scelerisque rutrum congue. Donec quis finibus erat, a maximus ligula. Nulla hendrerit magna turpis, ut porta metus ornare nec. In sit amet mattis justo. In id fermentum ante, nec aliquam dolor. Vivamus ut mauris tempus tellus vulputate pharetra. Maecenas laoreet leo sapien, a aliquam orci accumsan eu. Nunc quis nulla rhoncus, convallis lectus id, posuere erat. Integer sit amet metus nec risus eleifend porta nec eget odio."
 }];
 
 
